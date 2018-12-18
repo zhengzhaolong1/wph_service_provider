e911afd4ed68a41e28edea301c4fbb762cfeca28
zhengzhaolong <595264176@qq.com>
2018-12-18 19:30:47 +0800

    Initial commit

---
+++ src/test/java/com/example/wph_service_provider/WphServiceProviderApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.wph_service_provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WphServiceProviderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
